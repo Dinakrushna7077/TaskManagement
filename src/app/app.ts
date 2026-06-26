0b2acc865c7ab72d6e370ed0c44ed9e0f948ee54
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-06-26 19:24:37 +0530

    implement all function

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.css'
 })
 export class App {
-  //protected readonly title = signal('TaskManagement');
+  protected readonly title = signal('Task Management');
+  protected caption=signal("New Task");
   
 task:string="";
 date:string="";
@@ -18,21 +19,41 @@ message=signal("");
 msgClass="";
 updateId=0;
 taskList:{id:number,task:string,date:string,hh:number,mm:number}[]=[];
-addTask=()=>{
-  if(this.task.trim()!==""&& this.date.trim()!=="")
+formValidation=()=>{
+  if(this.task.trim()!=="")
   {
-    let i=this.taskList.length+1;
-    console.log(this.mm())
-    this.taskList.push({id:i,task:this.task.trim(),date:this.date,hh:this.hh(),mm:this.mm()});
-    this.setDefaultValue();
-    const taskInput = document.getElementById('taskInput') as HTMLInputElement;
-    taskInput.focus();
-    this.showMessage("New task created...","text-success");
+    if(this.date!=="")
+    {
+      if((this.mm()!==0&&this.hh()===0)||(this.mm()===0&&this.hh()!==0)||(this.mm()!==0&&this.hh()!==0))
+      {
+        return true;
+      }
+      else
+      {
+        this.showMessage("Please Enter Duration time","text-danger");
+        (document.getElementById('hhInput') as HTMLInputElement).focus();
+      }
+    }
+    else
+    {
+      this.showMessage("Please Enter Date","text-danger");
+      (document.getElementById('dateInput') as HTMLInputElement).focus();
+    }
   }
   else
   {
-    this.showMessage("Please fill all the fields","text-danger");
+    this.showMessage("Please Enter Task Name","text-danger");
+    (document.getElementById('taskInput') as HTMLInputElement).focus();
   }
+  return false;
+}
+addTask=()=>{
+    let i=this.taskList.length+1;
+    console.log(i);
+    this.taskList.push({id:i,task:this.task.trim(),date:this.date,hh:this.hh(),mm:this.mm()});
+    this.setDefaultValue();
+    (document.getElementById('taskInput') as HTMLInputElement).focus();
+    this.showMessage("New task created...","text-success");
 }
 setDefaultValue=()=>{
     this.task="";
@@ -50,13 +71,17 @@ showMessage = (msg: string,cls:string) => {
 }
 //Delete task
 deleteTask=(index:number)=>{
-  this.taskList.splice(index,1);
+  this.taskList.splice(index,1);  
   this.showMessage("Task Deleted Successfully...","text-success");
+  this.setDefaultValue();
+  (document.getElementById('taskInput') as HTMLInputElement).focus();
 }
 
 //edit task
 editTask=(i:number)=>{
-  const _task=this.taskList.find(t=>t.id=i);
+  let _task=this.taskList.find(t=>t.id==i);
+  console.log(_task);
+  console.log(this.taskList);
   if(_task!=null)
   {
     this.task=_task.task;
@@ -64,11 +89,13 @@ editTask=(i:number)=>{
     this.updateId=_task.id;
     this.hh.set(_task.hh);
     this.mm.set(_task.mm);
+    this.caption.set("Edit Task");
+    (document.getElementById('taskInput') as HTMLInputElement).focus();
   }
 }
 //update task
 updateTask=(updateId:number)=>
-{
+{ 
   const _task=this.taskList.find(t=>t.id=updateId);
   if(_task!=null)
   {
@@ -76,21 +103,33 @@ updateTask=(updateId:number)=>
     _task.task=this.task;
     _task.hh=this.hh();
     _task.mm=this.mm();
-    this.setDefaultValue()
     this.showMessage("Update Successfully...","text-success");
   }
   else
   {
-    this.setDefaultValue()
     this.showMessage("Task Unavailable...","text-danger");
   }
+  this.caption.set("New Task");
+  this.setDefaultValue();
+}
+//cancel update
+cancelEdit=()=>{
+  this.setDefaultValue();
+  this.updateId=0;
+  this.caption.set("New Task");
 }
 constructor()
 {
+      this.taskList.push(
+        {id:1,task:"Learning .Net Core",date:"2026-06-12",hh:1,mm:30},
+        {id:2,task:"Learning Angular",date:"2026-06-12",hh:2,mm:30},
+        {id:3,task:"Practice Query",date:"2026-06-12",hh:1,mm:0}
+      );
   effect(()=>{
+    this.mm.set(Math.floor(this.mm()))
     if(this.mm()>60)
     {
-      this.hh.set(Math.floor(this.mm()/60));
+      this.hh.set(this.hh()+Math.floor(this.mm()/60));
       this.mm.set(this.mm()%60);
     }
   })
